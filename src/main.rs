677d14d5d72c3b8bd13ff692c6898eefa684d3b9
weijia <646025053@qq.com>
2024-04-22 20:13:03 +0800

    Add bytes dependency and update process function

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,31 @@ use std::os::windows::process;
 
 use tokio::net::{TcpListener, TcpStream};
 use mini_redis::{Connection,Frame};
-
+use bytes::Bytes;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+type Db = Arc<Mutex<HashMap<String,Bytes>>>;
+// Arc is used to share data between threads
 #[tokio::main]
 async fn main(){
     // bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     
+    println!("Listening");
+    let db = Arc::new(Mutex::new(HashMap::new()));
+    
         loop{
             let (socket,_) =listener.accept().await.unwrap();
             // await will make situation to wait until the connection is made
             // process(socket).await;
+            let db = db.clone();
             tokio::spawn(async move{
-                process(socket).await;
+                process(socket,db).await;
                 // this will spawn a new task to process the connection
             });
         }
 }
-async fn process(socket: TcpStream){
+async fn process(socket: TcpStream,db: Db){
     // let mut connection = Connection::new(socket);
     // if let Some(frame) =connection.read_frame().await.unwrap(){
     //     println!("GOT: {:?}",frame);
@@ -26,19 +34,19 @@ async fn process(socket: TcpStream){
     //     connection.write_frame(&response).await.unwrap();
     // }
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-    // A hashmap is used to store data
-    let mut db = HashMap::new();
     let mut connection = Connection::new(socket);
     while let Some(frame) = connection.read_frame().await.unwrap(){
         let response = match Command::from_frame(frame).unwrap(){
             Set(cmd) => {
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                // set lock to write to the db
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) =>{
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key())  {
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                     
                 }else {
                     Frame::Null
