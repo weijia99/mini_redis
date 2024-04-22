66f31610e00911fd968a55d3d542ca9436fad593
weijia <646025053@qq.com>
2024-04-22 21:15:49 +0800

    Add mpsc in redis

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,12 +1,13 @@
 use bytes::Bytes;
-use mini_redis;
-use tokio::sync::mpsc;
+use mini_redis::{self, client};
+use tokio::sync::{mpsc,oneshot};
 // define one customer and N procedure queue
-
+type Responder<T> = oneshot::Sender<mini_redis::Result<T>>;
 #[derive(Debug)]
 enum Command{
     Get {
-        key:String
+        key:String,
+        // resp:Responder<Option<Bytes>>
     },
     Set{
         key: String,
@@ -18,19 +19,36 @@ enum Command{
 async fn main(){
     let (tx,mut rx) = mpsc::channel(32);
     // define a channel with a buffer size of 32
-    let tx2 =tx.clone();
-    // clone the transmitter ,so that we can use it in the multiple tasks
-    tokio::spawn(async move{
-        tx.send("send from first task").await;
+    let manager = tokio::spawn(async move{
+        let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+        while let Some(cmd) =rx.recv().await  {
+            use Command::*;
+            match cmd {
+                Get {key} => {
+                    client.get(&key).await;
+                }
+                Set { key, val } =>{
+                    client.set(&key,val);
+                }
+            }
+            
+        }
     });
-    tokio::spawn(async move{
-        tx2.send("send from second task").await;
+    // add 2 tasks 
+    let tx2 =tx.clone();
+    let t1= tokio::spawn(async move{
+        let cmd =Command::Get { key: "hello".to_string(), };
+        tx.send(cmd).await.unwrap();
+    });
+    let t2 = tokio::spawn(async move{
+        let cmd = Command::Set { key: "foo".to_string(), val: "bar".into(), };
+        tx2.send(cmd).await.unwrap();
     });
 
-    // read from the channel
-    while let Some(message) = rx.recv().await{
-        println!("GOT = {:?}",message);
-    }
+    // In a certain order, wait for the tasks to complete
+    t1.await.unwrap();
+    t2.await.unwrap();
+    manager.await.unwrap();
 
 }
 
